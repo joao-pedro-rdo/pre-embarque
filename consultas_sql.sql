91a6ea6cdb538d6416567c729a87b644c5c699ed
joaopemello <116198553+joaopemello@users.noreply.github.com>
2023-02-09 16:28:22 -0300

    Adição de 1 view e remoção de comentários

--- consultas_sql.sql
+++ consultas_sql.sql
@@ -1,4 +1,4 @@
-CREATE VIEW vw_total_operacoes_clientes AS--valor total de operacoes por clientes// consulta apenas em clientes pre-embarque
+CREATE VIEW vw_total_operacoes_clientes AS
 	SELECT nome, SUM(valor_de_operacao) AS total_cliente FROM usuario
 		INNER JOIN operacao_usuario
 			ON operacao_usuario.cnpj = usuario.cnpj
@@ -7,7 +7,7 @@ CREATE VIEW vw_total_operacoes_clientes AS--valor total de operacoes por cliente
 		GROUP BY nome;
 
 
-CREATE VIEW vw_info_operacao_usuario AS --consulta das informacoes da operacao e do cliente, ocultando cnpj do cliente e valores da operacao
+CREATE VIEW vw_info_operacao_usuario AS 
 	SELECT nome, porte, area_operacional, operacao.numero AS numero_da_operacao, 
 		data_de_contratacao, situacao_da_operacao, fontes_de_recurso_desembolsados,
 		modalidade_de_apoio, forma_apoio, inovacao, instrumento_financeiro FROM usuario
@@ -19,19 +19,19 @@ CREATE VIEW vw_info_operacao_usuario AS --consulta das informacoes da operacao e
 			ON operacao.numero = pre.numero;
 
 
-CREATE VIEW vw_clientes_pre_embarque AS	--consulta apenas os clientes de pre-embarque
+CREATE VIEW vw_clientes_pre_embarque AS
 	SELECT nome, cliente.cnpj FROM usuario
 		INNER JOIN cliente
 			ON usuario.cnpj = cliente.cnpj;
 
 
-CREATE VIEW vw_clientes_pos_embarque AS	--consulta apenas os clientes de pos-embarque
+CREATE VIEW vw_clientes_pos_embarque AS	
 	SELECT nome, exportador.cnpj FROM usuario
 		INNER JOIN exportador
 			ON usuario.cnpj = exportador.cnpj;
 
 
-CREATE VIEW vw_exportacoes_argentina AS --consulta nome cliente e numero da operacao em todas as operacoes que tem como pais destino a argentina 
+CREATE VIEW vw_exportacoes_argentina AS
 	SELECT nome, pos.numero AS numero_operacao, pais_destino FROM usuario
 		INNER JOIN operacao_usuario
 			ON usuario.cnpj = operacao_usuario.cnpj
@@ -39,7 +39,11 @@ CREATE VIEW vw_exportacoes_argentina AS --consulta nome cliente e numero da oper
 			ON operacao_usuario.numero = pos.numero
 		WHERE pais_destino = 'ARGENTINA';
 		
-CREATE VIEW vw_usuario_porte_grande AS --consulta apenas os usuarios de porte grande
+CREATE VIEW vw_usuario_porte_grande AS
 	SELECT nome, cnpj, porte FROM usuario
 		WHERE porte = 'GRANDE';
 
+CREATE VIEW vw_ocorrencias_clientes_uf AS
+	SELECT uf, COUNT(*) as ocorrencias_clientes FROM usuario
+		GROUP BY uf;
+		
